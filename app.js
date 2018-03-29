6ccc589eaf05adff5117507dae374d85d59aed16
Chester ONeill <c.oneill.codes@gmail.com>
2018-03-28 22:22:56 -0400

    Indent

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 let Discord = require('discord.io');
 
 let bot = new Discord.Client({
-	token: process.env.BOT_TOKEN,
-	autorun: true
+    token: process.env.BOT_TOKEN,
+    autorun: true
 });
 bot.on('ready', function() {
-	console.log('Logged in as %s - %s\n', bot.username, bot.id);
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
 bot.on('disconnect', function(errMsg, code) { console.log(errMsg, code) });
 bot.on('message', function(user, userID, channelID, message, event) {
-	if (message === "!roll") {
-		bot.sendMessage({
-			to: channelID,
-			message: Math.floor(Math.random() * 6) + 1
-		});
-	}
+    if (message === "!roll") {
+        bot.sendMessage({
+            to: channelID,
+            message: Math.floor(Math.random() * 6) + 1
+        });
+    }
 });
