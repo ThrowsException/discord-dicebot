3dbb59ac295ddcfc80b5a7f994d85db0bfe72ed9
Chester ONeill <c.oneill.codes@gmail.com>
2018-05-25 00:56:24 -0400

    Update post to json

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ bot.on('disconnect', function(errMsg, code) { console.log(errMsg, code) });
 bot.on('message', function(user, userID, channelID, message, event) {
     if (message.indexOf("!broadcast") === 0) {
         [ command, subject, body ] = message.split(' ')
-        request.post(`${process.env.AWEBER_API}/broadcast/${process.env.ACCOUNT_ID}`,
-            { subject: subject, message: body },
-            function(error, response, body) {
+        options = {
+            uri : `${process.env.AWEBER_API}/broadcast/${process.env.ACCOUNT_ID}`,
+            json: true,
+            body: { subject: subject, message: body }
+        };
+        request.post(options, function(error, response, body) {
             bot.sendMessage({
                 to: channelID,
                 message: body
