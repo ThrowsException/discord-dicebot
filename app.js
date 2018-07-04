c900b6ca53ad231688392497aa9aa6cd49c02e4b
Chester O'Neill <c.oneill.codes@gmail.com>
2018-07-04 18:08:03 -0400

    Use discord js

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-let Discord = require('discord.io');
+let Discord = require('discord.js');
 let request = require('request');
+var util = require('util');
 
-let bot = new Discord.Client({
-    token: process.env.BOT_TOKEN,
-    autorun: true
-});
-bot.on('ready', function() {
-    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+let bot = new Discord.Client();
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}`);
 });
 
-bot.on('disconnect', function(errMsg, code) { console.log(errMsg, code) });
-bot.on('message', function(user, userID, channelID, message, event) {
-    if (message.indexOf("!broadcast") === 0) {
-        [ command, subject, body ] = message.split(' ')
+bot.on('message', (message) => {
+    if (message.content.indexOf("!broadcast") === 0) {
+        [ command, subject, body ] = message.content.split(' ')
         options = {
             uri : `${process.env.AWEBER_API}/broadcast/${process.env.ACCOUNT_ID}`,
             json: true,
             body: { subject: subject, message: body }
         };
-        request.post(options, function(error, response, body) {
-            bot.sendMessage({
-                to: channelID,
-                message: body
-            });
+        request.post(options, (error, response, body) => {
+            if(error) {
+              console.error(error);
+            }
         });
     }
 });
+
+bot.login(process.env.BOT_TOKEN);
