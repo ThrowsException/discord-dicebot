c263ecfe5641adb526f5f0b3343e726065422887
Chester O'Neill <c.oneill.codes@gmail.com>
2018-07-05 00:05:45 -0400

    Send message by channel to aweber api

--- app.js
+++ app.js
@@ -8,19 +8,17 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-    if (message.content.indexOf("!broadcast") === 0) {
-        [ command, subject, body ] = message.content.split(' ')
-        options = {
-            uri : `${process.env.AWEBER_API}/broadcast/${process.env.ACCOUNT_ID}`,
-            json: true,
-            body: { subject: subject, message: body }
-        };
-        request.post(options, (error, response, body) => {
-            if(error) {
-              console.error(error);
-            }
-        });
-    }
+    const {content, channel} = message;
+    options = {
+        uri : `${process.env.AWEBER_API}/broadcast`,
+        json: true,
+        body: { subject: 'Update', message: content, channel: channel.id }
+    };
+    request.post(options, (error, response, body) => {
+        if(error) {
+          console.error(error);
+        }
+    });
 });
 
 bot.login(process.env.BOT_TOKEN);
